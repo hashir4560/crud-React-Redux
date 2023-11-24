a876a825b6d47df37e2248f15df13607e31782bf
hashir4560 <haashirraja@gmail.com>
2023-11-24 19:21:30 +0500

    Added User Reducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import "./App.css";
+import React from "react"; // Import React
+import { useDispatch, useSelector } from "react-redux"; // Fix the import
 
 function App() {
+  const userList = useSelector((state) => state.users.value);
+  const dispatch = useDispatch(); // Add useDispatch to dispatch actions
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="addUser">
+        <input type="text" placeholder="Name ..." />
+        <input type="text" placeholder="UserName..." />
+        <button>Add User </button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((user) => (
+          <div>
+            <h1>{user.name}</h1>
+            <h1>{user.username}</h1>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
+import usersReducer from "./features/Users";
 
 const store = configureStore({
   reducer: {
-    //users
+    users: usersReducer,
   },
 });
 
@@ -21,6 +22,6 @@ root.render(
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// to log results (for example: reportWebVitals(console.log))s
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
