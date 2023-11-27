433b2350d73ad5ca72a0e06a58d7b6dd2a13d35c
hashir4560 <haashirraja@gmail.com>
2023-11-27 16:19:13 +0500

    Update and Delete User Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
 import "./App.css";
-import React from "react"; // Import React
+import React, { useState } from "react"; // Import React
 import { useDispatch, useSelector } from "react-redux"; // Fix the import
+import { addUser, deleteUser, updateUsername } from "./features/Users";
 
 function App() {
   const userList = useSelector((state) => state.users.value);
   const dispatch = useDispatch(); // Add useDispatch to dispatch actions
-
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [newusername, setNewUsername] = useState("");
   return (
     <div className="App">
       <div className="addUser">
-        <input type="text" placeholder="Name ..." />
-        <input type="text" placeholder="UserName..." />
-        <button>Add User </button>
+        <input
+          type="text"
+          placeholder="Name ..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="UserName..."
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <button
+          onClick={() => {
+            const lastUserId =
+              userList.length > 0 ? userList[userList.length - 1].id : 0;
+            dispatch(
+              addUser({
+                id: lastUserId + 1,
+                name,
+                username,
+              })
+            );
+          }}
+        >
+          Add User
+        </button>
       </div>
       <div className="displayUsers">
         {userList.map((user) => (
           <div>
             <h1>{user.name}</h1>
             <h1>{user.username}</h1>
+            <input
+              type="text"
+              placeholder="New Username ..."
+              onChange={(event) => {
+                setNewUsername(event.target.value);
+              }}
+            />
+            <button
+              onClick={() => {
+                dispatch(
+                  updateUsername({ id: user.id, username: newusername })
+                );
+              }}
+            >
+              Update Username
+            </button>
+            <button
+              onClick={() => {
+                dispatch(deleteUser({ id: user.id }));
+              }}
+            >
+              Delete User
+            </button>
           </div>
         ))}
       </div>
--- src/features/Users.js
+++ src/features/Users.js
@@ -4,8 +4,21 @@ export const userSlice = createSlice({
   name: "users",
   initialState: { value: UsersData },
   reducers: {
-    addUser: (state, action) => {},
+    addUser: (state, action) => {
+      state.value.push(action.payload);
+    },
+    deleteUser: (state, action) => {
+      state.value = state.value.filter((user) => user.id !== action.payload.id);
+    },
+    updateUsername: (state, action) => {
+      state.value.flatMap((user) => {
+        if (user.id === action.payload.id) {
+          user.username = action.payload.username;
+        }
+      });
+    },
   },
 });
 
+export const { addUser, deleteUser, updateUsername } = userSlice.actions;
 export default userSlice.reducer;
